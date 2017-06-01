6d6c4315de4f6ce36e0bfa492a514d7f84aee16a
xjun <xiongjunceplj@163.com>
2017-06-01 18:40:31 +0800

    first commit

---
+++ common/config/bootstrap.php
@@ -0,0 +1,5 @@
+<?php
+Yii::setAlias('@common', dirname(__DIR__));
+Yii::setAlias('@passport', dirname(dirname(__DIR__)) . '/passport');
+Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
+Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
