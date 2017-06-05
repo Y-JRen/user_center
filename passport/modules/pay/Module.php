3ea5237438d4a6f520f3cdddeaa64d126b4cc002
xjun <xiongjunceplj@163.com>
2017-06-05 16:44:57 +0800

    代码更新

---
+++ passport/modules/pay/Module.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace passport\modules\pay;
+
+/**
+ * pay module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'passport\modules\pay\controllers';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
---
+++ passport/modules/pay/controllers/OrderController.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xiongjun
+ * Date: 2017/6/5
+ * Time: 16:36
+ */
+
+namespace passport\modules\pay\controllers;
+
+
+use passport\controllers\BaseController;
+
+/**
+ * 下单订单
+ *
+ * Class OrderController
+ *
+ * @package passport\modules\pay\controllers
+ */
+class OrderController extends BaseController
+{
+    /**
+     *
+     */
+    public function actionIndex()
+    {
+        
+    }
+}
