a94297c8e06dbae3fba094c393eab009ead7fd71
华伟 <619530038@qq.com>
2017-06-14 14:51:01 +0800

    添加无需图形的短信验证码

--- passport/modules/sso/controllers/SmsController.php
+++ passport/modules/sso/controllers/SmsController.php
@@ -8,23 +8,24 @@ use passport\modules\sso\models\SmsForm;
 
 class SmsController extends BaseController
 {
-	/**
-	 * @inheritdoc
-	 */
-	public function behaviors()
-	{
-		return ArrayHelper::merge(
-				parent::behaviors(), 
-				[
-					'verbs' => [
-							'class' => VerbFilter::className(),
-							'actions' => [
-								'get-msg' => ['get'],
-							],
-					],
-				]
-		);
-	}
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return ArrayHelper::merge(
+            parent::behaviors(),
+            [
+                'verbs' => [
+                    'class' => VerbFilter::className(),
+                    'actions' => [
+                        'get-msg' => ['get'],
+                    ],
+                ],
+            ]
+        );
+    }
+
     public function actionIndex()
     {
         
@@ -47,5 +48,22 @@ class SmsController extends BaseController
     	}else{
     		return $this->_error(997,$res['msg']);
     	}
+    }
+
+    /**
+     * 临时获取短信验证码，取出所有验证
+     * @return array
+     */
+    public function actionCode()
+    {
+        $model = new SmsForm();
+        $model->load(Yii::$app->request->get(), '');
+        $res = $model->send();
+
+        if ($res) {
+            return $this->_return('成功');
+        } else {
+            return $this->_error(997, $res['msg']);
+        }
     }
 }
