d5f94710469a40a1310301136186859527cb4a43
华伟 <619530038@qq.com>
2017-06-16 13:26:52 +0800

    添加检测用户接口

--- passport/modules/inside/controllers/UserController.php
+++ passport/modules/inside/controllers/UserController.php
@@ -15,6 +15,14 @@ use passport\helpers\Config;
 
 class UserController extends BaseController
 {
+    public function verbs()
+    {
+        return [
+            'reg' => ['POST'],
+            'check' => ['GET'],
+        ];
+    }
+
     /**
      * 用户注册
      */
@@ -36,11 +44,21 @@ class UserController extends BaseController
             $model->login_time = 0;
             $model->status = 1;
             if ($model->validate()) {
-                var_dump($model->attributes);exit;
                 return $this->_return(['uid' => $model->id]);
             } else {
                 return $this->_error(1002, current($model->getFirstErrors()));
             }
         }
+    }
+
+    /**
+     * 检测改uid的用户是否存在
+     * @param $uid
+     * @return array
+     */
+    public function actionCheck($uid)
+    {
+        $data = User::find()->where(['id' => $uid])->asArray()->exists();
+        return $this->_return($data);
     }
 }
