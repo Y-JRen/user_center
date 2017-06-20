a0e7aa6d8fc0e5134700a72b7533984484dbfa22
华伟 <619530038@qq.com>
2017-06-20 18:20:22 +0800

    添加第三方erp退款确认接口

---
+++ common/logic/RefundLogin.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: huawei
+ * Date: 2017/6/20
+ * Time: 14:17
+ */
+
+namespace common\logic;
+
+
+/**
+ * ERP退款确认接口
+ *
+ * @package common\logic
+ */
+class RefundLogin extends Logic
+{
+    /**
+     * 订单确认
+     */
+    public function orderConfirm()
+    {
+
+    }
+
+    /**
+     * 金额确认
+     */
+    public function amountConfirm()
+    {
+
+    }
+}
