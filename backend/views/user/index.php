852d5757dec72c5a4a9720313967d24b691991ed
华伟 <619530038@qq.com>
2017-07-25 19:28:02 +0800

    导出优化

--- backend/views/user/index.php
+++ backend/views/user/index.php
@@ -18,61 +18,60 @@ $this->registerJsFile('//cdn.jsdelivr.net/momentjs/latest/moment.min.js', ['depe
 $this->registerJsFile('//cdn.jsdelivr.net/bootstrap.daterangepicker/2/daterangepicker.js', ['depends' => 'yii\web\JqueryAsset']);
 $this->registerCssFile('//cdn.jsdelivr.net/bootstrap.daterangepicker/2/daterangepicker.css', ['depends' => 'yii\bootstrap\BootstrapAsset']);
 ?>
-<div class="user-index">
+    <div class="user-index">
 
-    <p>
-        <?= Html::a('导出', '', ['class' => 'btn btn-primary', 'data-method' => 'HEAD']) ?>
-    </p>
+        <p>
+            <?= Html::a('导出', '', ['class' => 'btn btn-primary', 'data-method' => 'HEAD']) ?>
+        </p>
+        <?= GridView::widget([
+            'dataProvider' => $dataProvider,
+            'filterModel' => $searchModel,
+            'columns' => [
+                ['class' => 'yii\grid\SerialColumn'],
 
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-
-            [
-                'attribute' => 'phone',
-                'format' => 'raw',
-                'value' => function ($model) {
-                    return Html::a($model->phone, ['/user/order', 'uid' => $model->id]);
-                }
-            ],
-            'user_name',
-            'email:email',
-            [
-                'attribute' => 'status',
-                'value' => function ($model) {
-                    return $model->status == 1 ? '正常' : '禁用';
-                },
-            ],
-            [
-                'attribute' => 'from_platform',
-                'value' => function ($model) {
-                    return ArrayHelper::getValue(Config::getPlatformArray(), $model->from_platform);
-                },
-            ],
-            'from_channel',
-            'reg_time:datetime',
-            'reg_ip',
-            'login_time:datetime',
-
-            [
-                'class' => 'yii\grid\ActionColumn',
-                'template' => '{view} {order_view} {amount_view}',
-                'buttons' => [
-                    'view' => function ($url, $model, $key) {
-                        return Html::a('详情', ['user/view', 'id' => $model->id], ['class' => 'btn btn-success btn-xs']);
-                    },
-                    'order_view' => function ($url, $model, $key) {
-                        return Html::a('订单详情', ['user/order', 'uid' => $model->id], ['class' => 'btn btn-success btn-xs']);
-                    },
-                    'amount_view' => function ($url, $model, $key) {
-                        return Html::a('资金详情', ['user/amount', 'uid' => $model->id], ['class' => 'btn btn-success btn-xs']);
+                [
+                    'attribute' => 'phone',
+                    'format' => 'raw',
+                    'value' => function ($model) {
+                        return Html::a($model->phone, ['/user/order', 'uid' => $model->id]);
                     }
-                ]
+                ],
+                'user_name',
+                'email:email',
+                [
+                    'attribute' => 'status',
+                    'value' => function ($model) {
+                        return $model->status == 1 ? '正常' : '禁用';
+                    },
+                ],
+                [
+                    'attribute' => 'from_platform',
+                    'value' => function ($model) {
+                        return ArrayHelper::getValue(Config::getPlatformArray(), $model->from_platform);
+                    },
+                ],
+                'from_channel',
+                'reg_time:datetime',
+                'reg_ip',
+                'login_time:datetime',
+
+                [
+                    'class' => 'yii\grid\ActionColumn',
+                    'template' => '{view} {order_view} {amount_view}',
+                    'buttons' => [
+                        'view' => function ($url, $model, $key) {
+                            return Html::a('详情', ['user/view', 'id' => $model->id], ['class' => 'btn btn-success btn-xs']);
+                        },
+                        'order_view' => function ($url, $model, $key) {
+                            return Html::a('订单详情', ['user/order', 'uid' => $model->id], ['class' => 'btn btn-success btn-xs']);
+                        },
+                        'amount_view' => function ($url, $model, $key) {
+                            return Html::a('资金详情', ['user/amount', 'uid' => $model->id], ['class' => 'btn btn-success btn-xs']);
+                        }
+                    ]
+                ],
             ],
-        ],
-    ]); ?>
+        ]); ?>
     </div>
 <?php
 $js = <<<JS
@@ -88,6 +87,7 @@ $js = <<<JS
             });
               $('input[name="UserSearch[reg_time]"]').on('apply.daterangepicker', function(ev, picker) {
                   $(this).val(picker.startDate.format('YYYY-MM-DD') + ' - ' + picker.endDate.format('YYYY-MM-DD'));
+                  
               });
             
               $('input[name="UserSearch[reg_time]"]').on('cancel.daterangepicker', function(ev, picker) {
