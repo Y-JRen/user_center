b48a4d0993297b3029884d894bb45c6aac08480d
华伟 <619530038@qq.com>
2017-07-25 15:37:20 +0800

    添加短信发送通道

--- common/logic/HttpLogic.php
+++ common/logic/HttpLogic.php
@@ -1,8 +1,9 @@
 <?php
+
 namespace common\logic;
 
 /**
- * http 
+ * http
  * Class ApiLogsLogic
  * @package api\logic
  */
@@ -13,55 +14,62 @@ class HttpLogic extends Logic
     public $connecttimeout = 30;
     public $timeout = 30;
     public $ssl_verifypeer = FALSE;
-    
-    
-	function http($url, $method, $postfields = NULL, $headers = array()) {
-	    $ci = curl_init();
-	    /* Curl settings */
-	    curl_setopt($ci, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_0);
-	    curl_setopt($ci, CURLOPT_CONNECTTIMEOUT, $this->connecttimeout);
-	    curl_setopt($ci, CURLOPT_TIMEOUT, $this->timeout);
-	    curl_setopt($ci, CURLOPT_RETURNTRANSFER, TRUE);
-	    curl_setopt($ci, CURLOPT_ENCODING, "");
-	    curl_setopt($ci, CURLOPT_SSL_VERIFYPEER, $this->ssl_verifypeer);
-	    curl_setopt($ci, CURLOPT_HEADER, FALSE);
-	
-	    switch ($method) {
-	        case 'POST':
-	            curl_setopt($ci, CURLOPT_POST, TRUE);
-	            if (!empty($postfields)) {
-	                curl_setopt($ci, CURLOPT_POSTFIELDS, $postfields);
-	            }
-	            break;
-	        case 'DELETE':
-	            curl_setopt($ci, CURLOPT_CUSTOMREQUEST, 'DELETE');
-	            if (!empty($postfields)) {
-	                $url = "{$url}?{$postfields}";
-	            }
-	    }
-	
-	    curl_setopt($ci, CURLOPT_URL, $url );
-	    curl_setopt($ci, CURLOPT_HTTPHEADER, $headers );
-	    curl_setopt($ci, CURLINFO_HEADER_OUT, TRUE );
-	
-	    $response = curl_exec($ci);
-	
-	    if ($this->debug) {
-	        echo "=====post data======\r\n";
-	        var_dump($postfields);
-	
-	        echo "=====headers======\r\n";
-	        print_r($headers);
-	
-	        echo '=====request info====='."\r\n";
-	        print_r( curl_getinfo($ci) );
-	
-	        echo '=====response====='."\r\n";
-	        print_r( $response );
-	    }
-	    curl_close ($ci);
-	    return $response;
-	}
-    
-   
+    public $setopt_param = [];
+
+
+    function http($url, $method, $postfields = NULL, $headers = array())
+    {
+        $ci = curl_init();
+        /* Curl settings */
+        curl_setopt($ci, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_0);
+        curl_setopt($ci, CURLOPT_CONNECTTIMEOUT, $this->connecttimeout);
+        curl_setopt($ci, CURLOPT_TIMEOUT, $this->timeout);
+        curl_setopt($ci, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ci, CURLOPT_ENCODING, "");
+        curl_setopt($ci, CURLOPT_SSL_VERIFYPEER, $this->ssl_verifypeer);
+        curl_setopt($ci, CURLOPT_HEADER, FALSE);
+
+        if (!empty($this->setopt_param)) {
+            curl_setopt_array($ci, $this->setopt_param);
+        }
+
+        switch ($method) {
+            case 'POST':
+                curl_setopt($ci, CURLOPT_POST, TRUE);
+                if (!empty($postfields)) {
+                    curl_setopt($ci, CURLOPT_POSTFIELDS, $postfields);
+                }
+                break;
+            case 'DELETE':
+                curl_setopt($ci, CURLOPT_CUSTOMREQUEST, 'DELETE');
+                if (!empty($postfields)) {
+                    $url = "{$url}?{$postfields}";
+                }
+                break;
+        }
+
+        curl_setopt($ci, CURLOPT_URL, $url);
+        curl_setopt($ci, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ci, CURLINFO_HEADER_OUT, TRUE);
+
+        $response = curl_exec($ci);
+
+        if ($this->debug) {
+            echo "=====post data======\r\n";
+            var_dump($postfields);
+
+            echo "=====headers======\r\n";
+            print_r($headers);
+
+            echo '=====request info=====' . "\r\n";
+            print_r(curl_getinfo($ci));
+
+            echo '=====response=====' . "\r\n";
+            print_r($response);
+        }
+        curl_close($ci);
+        return $response;
+    }
+
+
 }
