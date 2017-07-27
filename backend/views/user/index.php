5b7180eb3f8f109a661bd5aacb55762b4c853584
hj <1058619299@qq.com>
2017-07-27 14:54:50 +0800

    修改bug

--- backend/views/user/index.php
+++ backend/views/user/index.php
@@ -3,22 +3,41 @@
 use passport\helpers\Config;
 use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
-use yii\grid\GridView;
+//use yii\grid\GridView;
+use yii\widgets\ActiveForm;
 use yii\widgets\Pjax;
+use backend\grid\FilterColumn;
+use backend\grid\GridView;
+use common\models\User;
+
 
 /* @var $this yii\web\View */
 /* @var $searchModel backend\models\search\UserSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
 $this->title = '用户管理';
-$this->params['breadcrumbs'][] = $this->title;
+$this->registerJsFile('/dist/plugins/daterangepicker/moment.min.js', [
+    'depends' => ['backend\assets\AdminLteAsset']
+]);
+$this->registerJsFile('/dist/plugins/daterangepicker/daterangepicker.js', [
+    'depends' => ['backend\assets\AdminLteAsset']
+]);
+$this->registerJsFile('/dist/js/user/date.js', [
+    'depends' => ['backend\assets\AdminLteAsset']
+]);
 ?>
-<div class="user-index">
 
+    <?php Pjax::begin(); ?>
 
-    <?php Pjax::begin(); ?>    <?= GridView::widget([
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+    ]); ?>
+
+    <?= $this->render('_search2', ['model' => $searchModel]) ?>
+
+    <?= GridView::widget([
         'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
 
@@ -26,27 +45,41 @@ $this->params['breadcrumbs'][] = $this->title;
                 'attribute' => 'phone',
                 'format' => 'raw',
                 'value' => function ($model) {
-                    return Html::a($model->phone, ['/user/order', 'uid' => $model->id]);
+                    return Html::a($model->phone, ['/user/view', 'uid' => $model->id]);
                 }
             ],
             'user_name',
             'email:email',
             [
+                'class' => FilterColumn::className(),
                 'attribute' => 'status',
                 'value' => function ($model) {
-                    return $model->status == 1 ? '正常' : '禁用';
+                    return ArrayHelper::getValue(User::$statusArray,$model->status);
                 },
+                'filterArray' => User::$statusArray,
             ],
             [
+                'class' => FilterColumn::className(),
                 'attribute' => 'from_platform',
                 'value' => function ($model) {
                     return ArrayHelper::getValue(Config::getPlatformArray(), $model->from_platform);
                 },
+                'filterArray' => Config::getPlatformArray()
             ],
             'from_channel',
-            'reg_time:datetime',
+            [
+                'attribute' => 'reg_time',
+                'label' => '注册时间',
+                'format' => 'datetime',
+                'enableSorting' => true
+            ],
             'reg_ip',
-            'login_time:datetime',
+            [
+                'attribute' => 'login_time',
+                'label' => '最后登录时间',
+                'format' => 'datetime',
+                'enableSorting' => true
+            ],
 
             [
                 'class' => 'yii\grid\ActionColumn',
@@ -65,4 +98,5 @@ $this->params['breadcrumbs'][] = $this->title;
             ],
         ],
     ]); ?>
-    <?php Pjax::end(); ?></div>
+    <?php ActiveForm::end(); ?>
+    <?php Pjax::end(); ?>
