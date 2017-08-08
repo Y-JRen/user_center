cf2e953d13ca182f454b65430b2e8a9ddd1d4403
huawei <619530038@qq.com>
2017-08-08 14:12:34 +0800

    添加退款接口

--- passport/modules/inside/controllers/RefundController.php
+++ passport/modules/inside/controllers/RefundController.php
@@ -3,54 +3,48 @@
  * Created by PhpStorm.
  * User: legend
  * Date: 2017/8/8
- * Time: 上午9:48
+ * Time: 下午2:07
  */
 
 namespace passport\modules\inside\controllers;
 
+
 use common\models\PoolBalance;
-use passport\modules\inside\models\Order;
+use passport\models\Order;
 use Yii;
 use yii\base\Exception;
 
-
-/**
- * 内部消费接口
- *
- * 内部消费接口不走先冻结再解冻流程，直接扣除余额
- *
- * @package passport\modules\inside\controllers
- */
-class ConsumeController extends BaseController
+class RefundController extends BaseController
 {
-    /**
-     * @return array
-     * @throws Exception
-     */
+
     public function actionIndex()
     {
-        return false;
         $param = Yii::$app->request->post();
-        if ($param['order_type'] != Order::TYPE_CONSUME) {
+        if ($param['order_type'] != Order::TYPE_REFUND) {
             return $this->_error(2007);
         }
         $model = new Order();
         $param['notice_status'] = 4;
         if ($model->load($param, '') && $model->save()) {
-
             $transaction = Yii::$app->db->beginTransaction();
             try {
-                if (!$model->userBalance->less($model->amount)) {
-                    throw new Exception('扣除用户余额失败');
+                if (!$this->refundCheck($model->platform_order_id)) {
+                    throw new Exception('该笔订单，已有退款记录');
                 }
 
-                if (!$model->addPoolBalance(PoolBalance::STYLE_LESS)) {
+                if (!$model->userBalance->plus($model->amount)) {
+                    throw new Exception('余额增加失败');
+                }
+
+                // 添加资金流水记录
+                if (!$model->addPoolBalance(PoolBalance::STYLE_PLUS)) {
                     throw new Exception('添加资金流水记录失败');
                 }
 
                 if (!$model->setOrderSuccess()) {
                     throw new Exception('更新消费订单状态失败');
                 }
+
                 $transaction->commit();
 
                 $data['platform_order_id'] = $model->platform_order_id;
@@ -61,10 +55,30 @@ class ConsumeController extends BaseController
             } catch (Exception $e) {
                 $model->exceptionHandle();
                 $transaction->rollBack();
+                $model->setOrderFail();
                 throw $e;
             }
         } else {
-            return $this->_error(2101, $model->errors);
+            return $this->_error(2201, $model->errors);
         }
+    }
+
+
+    /**
+     * 查看该电商订单号是否有过退款操作
+     * 为true时可以退款
+     *
+     * @param $platform_order_id
+     * @return bool
+     */
+    public function refundCheck($platform_order_id)
+    {
+        $count = Order::find()->where([
+            'order_type' => Order::TYPE_REFUND,
+            'platform_order_id' => $platform_order_id,
+            'status' => Order::STATUS_SUCCESSFUL,
+        ])->count();
+
+        return empty($count);
     }
 }
