5017a2e9533d2e14356478779b7b32cba7782381
huawei <619530038@qq.com>
2017-08-23 13:50:00 +0800

    错误提示优化

--- passport/modules/inside/controllers/ConsumeController.php
+++ passport/modules/inside/controllers/ConsumeController.php
@@ -63,7 +63,7 @@ class ConsumeController extends BaseController
                 throw $e;
             }
         } else {
-            return $this->_error(2101, $model->errors);
+            return $this->_error(2101, current($model->getFirstErrors()));
         }
     }
 }
