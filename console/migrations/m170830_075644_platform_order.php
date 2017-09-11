d85e4185f7ad75337f20194d7aa670a08b76d93d
huawei <619530038@qq.com>
2017-09-11 13:30:33 +0800

    添加数据库脚本

---
+++ console/migrations/m170830_075644_platform_order.php
@@ -0,0 +1,26 @@
+<?php
+
+use yii\db\Migration;
+
+class m170830_075644_platform_order extends Migration
+{
+    public function safeUp()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+
+        $this->createTable('{{%platform_order}}', [
+            'id' => $this->primaryKey()->notNull(),
+            'uid' => $this->integer()->notNull(),
+            'platform_order_id' => $this->string(32)->notNull(),
+            'created_at' => $this->integer()->notNull(),
+        ], $tableOptions);
+    }
+
+    public function safeDown()
+    {
+        $this->dropTable('{{%platform_order}}');
+    }
+}
