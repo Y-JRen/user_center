e9c35676cb2db46175c1bfe40f29c48718264e84
huawei <619530038@qq.com>
2017-09-18 18:36:28 +0800

    Update

--- passport/modules/pay/logic/AlipayLogic.php
+++ passport/modules/pay/logic/AlipayLogic.php
@@ -70,7 +70,7 @@ class AlipayLogic extends Logic
 
         if ($html) {
             Redis::setAlipayOrderHtml($order->order_id, $html);
-            $result['data']['url'] = $url = Url::to(['/alipay/show', 'orderId' => $order->order_id], true);
+            $result['data']['qrCodeData'] = $result['data']['url'] = $url = Url::to(['/alipay/show', 'orderId' => $order->order_id], true);
             $result['data']['qrcode'] = Url::to(['/default/qrcode', 'url' => $url], true);
         } else {
             $result['status'] = 2004;
